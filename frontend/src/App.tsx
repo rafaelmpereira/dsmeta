c49883ee8fa137eb706a41e0c464cf7a00afce8e
Rafael Machado <rafaelmpereira.rmp@gmail.com>
2022-09-07 22:29:45 -0300

    Project cleaner

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
-
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
+	  /*<div>
+		<h1>Olá, Mundo!</h1>
+	  </div>
+	  */
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src="/vite.svg" className="logo" alt="Vite logo" />
