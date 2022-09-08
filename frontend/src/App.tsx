4590c0c827de8a05ae1722e7bbc22aa6d335584f
Rafael Machado <rafaelmpereira.rmp@gmail.com>
2022-09-07 23:54:05 -0300

    New Component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,32 +1,10 @@
-function App() {
-  const [count, setCount] = useState(0)
+import NotificationButton from "./components/NotificationButton"
+import "./styles.css"
 
+function App() {
   return (
     <div className="App">
-	  /*<div>
-		<h1>Olá, Mundo!</h1>
-	  </div>
-	  */
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+		<NotificationButton/>
     </div>
   )
 }
